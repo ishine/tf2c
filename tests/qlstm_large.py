eb6c726ff6ba42060bea83361f7f6e0ae6ae47f3
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2017-06-14 03:05:07 +0900

    Add qlstm_large

---
+++ tests/qlstm_large.py
@@ -0,0 +1,21 @@
+import tensorflow as tf
+
+
+def gen_graph():
+    i = tf.Variable(tf.random_normal([100, 1024]), name='i')
+    j = tf.Variable(tf.random_normal([100, 1024]), name='j')
+    f = tf.Variable(tf.random_normal([100, 1024]), name='f')
+    o = tf.Variable(tf.random_normal([100, 1024]), name='o')
+    c = tf.Variable(tf.random_normal([100, 1024]), name='c')
+
+    i = tf.sigmoid(i)
+    j = tf.tanh(i)
+    f = tf.sigmoid(i)
+    o = tf.sigmoid(i)
+    nc = tf.clip_by_value(c * f + j * i, -0.5, 0.5)
+    nh = tf.multiply(nc, o, name='result')
+    return nh
+
+
+def gen_model(sess):
+    sess.run(tf.global_variables_initializer())
